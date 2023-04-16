5b5f171333047466e2f63bd48c4971a338812e9c
Dexkod <ivanov.misha14@mail.ru>
2023-04-16 18:50:47 +0300

    Pages have been made and the Index and AddDay pages have been converted

---
+++ Presentation/Pages/AddDay.cshtml.cs
@@ -0,0 +1,119 @@
+using Domain.Entities;
+using Infrastructure.DbContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Presentation
+{
+    public class AddDayModel : PageModel
+    { 
+        public static List<Lesson> Lessons = new List<Lesson>();
+        public string ErrorMessageAddDay = "";
+        public string ErrorMessageLesson = "";
+        public AddDayModel()
+        {
+            
+        }
+        public void OnGet()
+        {
+        }
+
+        public void OnPostAddDay(string DateDay)
+        {
+            if(DateDay == null)
+            {
+                ErrorMessageAddDay = "Введите дату";
+                return;
+            }
+            ErrorMessageAddDay = "";
+            
+            SchoolContext context = new();
+
+            int[] dayMouthYear = DateDay.Split('-').Select(x => Convert.ToInt32(x)).ToArray();
+            var date = new DateTime(dayMouthYear[0], dayMouthYear[1], dayMouthYear[2]); 
+            var day = new Day(date.DayOfWeek.ToString(), date);
+
+            Lessons = Lessons.OrderBy(x => x.HoursAndMinutes.Hour).ToList();
+
+            foreach(var lesson in Lessons)
+            {
+                day.Lessons.Add(lesson);
+                lesson.Day = day;
+                lesson.DayId = day.Id;
+                context.Add(lesson);
+            }
+
+            context.Days.Add(day);
+            context.SaveChangesAsync();
+
+            Lessons = new();
+        }
+
+        public void OnPostAddLesson(string TimeNewLesson, string NameNewLesson, string PlaceNewLesson)
+        {
+            if(TimeNewLesson == null || NameNewLesson == null || PlaceNewLesson == null)
+            {
+                ErrorMessageLesson = "При создании пары произошли ошибка, заполните все поля";
+                return;
+            }
+            ErrorMessageLesson = "";
+
+            DateTime time = new();
+            time = time.AddHours(Convert.ToInt32(TimeNewLesson.Split(':', ' ')[0]));
+            time = time.AddMinutes(Convert.ToInt32(TimeNewLesson.Split(':', ' ')[1]));
+
+            Lesson lesson = new(NameNewLesson, PlaceNewLesson, time);
+            Lessons.Add(lesson);
+        }
+
+        public void OnPostPutLessons(string[] NameLessons, string[] Times,
+            string[] Places, string[] IsRemove)
+        {
+            if (NameLessons.Any(word => word.IsNullOrEmpty()) ||
+                Places.Any(word => word.IsNullOrEmpty()) ||
+                Times.Any(word => word.IsNullOrEmpty()))
+            {
+                ErrorMessageLesson = "При изменении пар произошла ошибка,введите все поля";
+                return;
+            }
+            ErrorMessageLesson = "";
+
+            char[] separators = { ' ', ':', '-' };
+
+            for(int i = 0; i < NameLessons.Length; i++)
+            {
+                try
+                {
+                    string[] HoursAndMinute = Times[i].Split(separators)
+                        .Select(number => string.Join("",number.Where(x => char.IsNumber(x))))
+                        .Where(number => !number.IsNullOrEmpty()).ToArray();
+                    DateTime time = new();
+                    time = time.AddHours(Convert.ToInt32(HoursAndMinute[0]));
+                    time = time.AddMinutes(Convert.ToInt32(HoursAndMinute[1]));
+
+                    Lessons[i].Name = NameLessons[i];
+                    Lessons[i].HoursAndMinutes = time;
+                    Lessons[i].Place = Places[i]; 
+                }
+                catch
+                {
+                    ErrorMessageLesson = "Не все изменения прошли успешно, попытайтесь снова";
+                }
+            }
+
+            for(int i = 0; i < IsRemove.Length; i++)
+            {
+                try
+                {
+                    Lessons.Remove(Lessons.FirstOrDefault(x => x.Id.ToString().Equals(IsRemove[i])));
+                }
+                catch
+                {
+                    ErrorMessageLesson = "Не все изменения прошли успешно, попытайтесь снова";
+                }
+            }
+
+        }
+    }
+}
--- Presentation/Pages/Index.cshtml.cs
+++ Presentation/Pages/Index.cshtml.cs
@@ -3,55 +3,134 @@ using Infrastructure.DbContext;
 using Infrastructure.FirstData;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Tokens;
 
 namespace SchoolElectronicMagazine.Pages
 {
     public class IndexModel : PageModel
     {
+        public static Day PutDay = new Day();
+        public string ErrorMessagePostLesson = string.Empty;
+        public string ErrorMessageLesson = "";
         private readonly ILogger<IndexModel> _logger;
-        public List<Day> Days = new List<Day>();
-        private SchoolContext schoolContext = new SchoolContext();
+        public List<Day> Days;
+        private SchoolContext schoolContext = new();
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-            
+            SearchWeek();
         }
 
         public void OnGet()
         {
-            //AddData.Add();
-            SearchWeek();
+            
         }
 
-        public void OnPost(string DayId, string TimeNewLesson,string PlaceNewLesson, string NameNewLesson)
+        public void OnPostAddLesson(string DayId, string TimeNewLesson,
+            string PlaceNewLesson, string NameNewLesson)
         {
-            //new L
-            Console.WriteLine();
+            if(DayId == null || TimeNewLesson == null || PlaceNewLesson== null ||
+               NameNewLesson == null)
+            {
+                ErrorMessagePostLesson = "При создании произошла ошибка, введите все поля";
+                return;
+            }
+            ErrorMessagePostLesson = "";
+
+            Day day = Days.FirstOrDefault(x => x.Id.ToString().Equals(DayId));
+
+            DateTime time = new();
+            time = time.AddHours(Convert.ToInt32(TimeNewLesson.Split(':',' ')[0]));
+            time = time.AddMinutes(Convert.ToInt32(TimeNewLesson.Split(':', ' ')[1]));
+
+            Lesson lesson = new(NameNewLesson,PlaceNewLesson, time);
+            day.Lessons.Add(lesson);
+            lesson.DayId = day.Id;
+            lesson.Day = day;
+
+            day.Lessons = day.Lessons.OrderBy(x => x.HoursAndMinutes.Hour).ToList();
+            schoolContext.Add(lesson);
+            schoolContext.SaveChanges();
+
+            Response.Redirect(Request.Path);
+        }
+
+        public void OnPostPutLessons(string[] NameLessons, string[] Times,
+            string[] Places, string[] IsRemove)
+        {
+            Day day = schoolContext.Days.FirstOrDefault(x => x.Id.Equals(PutDay.Id));
+
+            if (NameLessons.Any(word => word.IsNullOrEmpty()) ||
+                Places.Any(word => word.IsNullOrEmpty()) ||
+                Times.Any(word => word.IsNullOrEmpty()))
+            {
+                ErrorMessageLesson = "При изменении пар произошла ошибка,введите все поля";
+                return;
+            }
+            ErrorMessageLesson = "";
+
+            char[] separators = { ' ', ':', '-' };
+            int index = 0;
+
+            foreach (var lesson in day.Lessons)
+            {
+                try
+                {
+                    string[] HoursAndMinute = Times[index].Split(separators)
+                        .Select(number => string.Join("", number.Where(x => char.IsNumber(x))))
+                        .Where(number => !number.IsNullOrEmpty()).ToArray();
+                    DateTime time = new();
+                    time = time.AddHours(Convert.ToInt32(HoursAndMinute[0]));
+                    time = time.AddMinutes(Convert.ToInt32(HoursAndMinute[1]));
+
+                    lesson.Name = NameLessons[index];
+                    lesson.HoursAndMinutes = time;
+                    lesson.Place = Places[index];
+                }
+                catch
+                {
+                    ErrorMessageLesson = "Не все изменения прошли успешно, попытайтесь снова";
+                }
+                
+                index++;
+            }
+
+            for (int i = 0; i < IsRemove.Length; i++)
+            {
+                try
+                {
+                    Lesson lesson = day.Lessons
+                        .FirstOrDefault(x => x.Id.ToString().Equals(IsRemove[i]));
+
+                    day.Lessons.Remove(lesson);
+                    schoolContext.Lessons.Remove(lesson);
+                }
+                catch(Exception error)
+                {
+                    ErrorMessageLesson = "Не все изменения прошли успешно, попытайтесь снова";
+                }
+            }
+            PutDay = new Day();
+
+            schoolContext.SaveChanges();
+        }
+
+        public void OnPostGetDay(string DayId)
+        {
+            PutDay = Days.FirstOrDefault(x => x.Id.ToString().Equals(DayId));
         }
 
         private void SearchWeek()
         {
             // Last day in schedule
-            DateTime dayLast = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            DateTime dayLast = new(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
             dayLast = dayLast.AddDays(4);
             List<Day> allDays = schoolContext.Days.ToList();
             List<Lesson> lessons = schoolContext.Lessons.ToList();
 
             allDays = allDays.Where(x => (dayLast - x.Time).Days >= 0 && 
                 (dayLast - x.Time).Days < 7).ToList();
-            
-            for(int i = 0; i < allDays.Count - 1; i++)
-            {
-				for (int j = i + 1; j < allDays.Count - 1; j++)
-				{
-                    if(allDays[i].Time > allDays[j].Time)
-                    {
-                        var swap = allDays[i];
-                        allDays[i] = allDays[j];
-                        allDays[j] = swap;
-                    }
-				}
-			}
+            allDays = allDays.OrderBy(x => x.Time).ToList();
 
             Days = allDays;
         }
